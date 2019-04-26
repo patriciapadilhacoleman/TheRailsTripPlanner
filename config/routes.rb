1f067af6346c330cbe421e95ae65d6e097dedce0
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-04-26 16:41:51 -0700

    Added functionality to trip

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    get '/trips', to: 'trips#index'
+    get '/trips/new', to: 'trips#new', as: 'new_trip'
+    post '/trips', to: 'trips#create'
+    get '/trips/:id', to: 'trips#show', as: 'trip'
+    get '/trips/:id/edit', to: 'trips#edit', as: 'edit_trip'
+    patch '/trips/:id', to: 'trips#update'
+    delete '/trips/:id', to: 'trips#destroy'
+
+    root 'trips#index'
+
 end
