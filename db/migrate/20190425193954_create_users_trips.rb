c6efe7eabe24bbbbcb37a860821775a1c1e86588
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-04-26 10:54:25 -0700

    Removed all migrations

--- db/migrate/20190425193954_create_users_trips.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsersTrips < ActiveRecord::Migration[5.2]
-  def change
-    create_table :users_trips do |t|
-      t.boolean :admin
-      t.references :user, foreign_key: true
-      t.references :trip, foreign_key: true
-    end
-  end
-end
