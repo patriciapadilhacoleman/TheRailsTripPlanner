65b7570f91d5089356b32bb8da4833f49718a240
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-04-27 13:49:13 -0700

    Password authentication and signin working with bcrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
 class SessionsController < ApplicationController
   def new
-  
+
   end
 
   def create
 
-    binding.pry
-    user = User.find_by(name: params[:user][:name])
+
+    user = User.find_by(username: params[:user][:username])
 
     user = user.try(:authenticate, params[:user][:password])
+
 
     return redirect_to(controller: 'sessions', action: 'new') unless user
 
