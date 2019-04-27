35e86eb68c77facbcbcc0e2c5d05b0c2c5604948
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-04-27 13:11:48 -0700

    Added User Sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
-  before_action :require_logged_in
-  before_action :user_is_admin
+  # before_action :require_logged_in
+  # before_action :user_is_admin
   def new
 
   end
 
   def create
     @user = User.create(user_params)
+    binding.pry
     return redirect_to controller: 'users', action: 'new' unless @user.save
     session[:user_id] = @user.id
     redirect_to controller: 'welcome', action: 'home'
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(:username, :password, :password_confirmation,:first_name, :last_name, :email, :phone, :home_airport, :street_address, :city, :state, :zip)
   end
 
   def user_is_admin
